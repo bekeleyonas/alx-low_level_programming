bffc5e5c50813559d1248bd8cbdd44a9373ca0a3
Yonas Bekele <yonassbekele@gmail.com>
2022-07-04 19:19:38 -0700

    Two digit comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int ch, ch2;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		for (ch2 = ch + 1; ch2 <= 57; ch2++)
+		{
+			putchar(ch);
+			putchar)ch2);
+			if ((ch != 56) && (ch2 != 57))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
