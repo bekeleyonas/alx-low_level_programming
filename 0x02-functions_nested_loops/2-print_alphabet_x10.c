9036523765fa0ef6c287ae7434112165bf719b77
Yonas Bekele <yonassbekele@gmail.com>
2022-07-05 03:46:44 -0700

    Function a to z 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,9 +16,14 @@ void print_alphabet_x10(void)
 {
 
 	int i;
+	char ch;
 
 	for (i = 1; i <= 10; i++)
 	{
-		print_alphabet();
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
 	}
 }
