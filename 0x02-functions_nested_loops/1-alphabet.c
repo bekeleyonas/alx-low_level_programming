11fbd9216682a55ba2c649a8931019bbb6cb82c1
Yonas Bekele <yonassbekele@gmail.com>
2022-07-05 03:08:21 -0700

    Function print_alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
 /* more headers goes there */
 /* Below is the function definition for a function that prints a to z */
 
-void print_alphabet(void);
+void print_alphabet();
 
 /* betty style doc for function main goes there */
 /**
