d479bc997510861520c2e29a929bef2276d22639
Yonas Bekele <yonassbekele@gmail.com>
2022-07-05 03:43:20 -0700

    Function a to z 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/* more headers goes there */
+/* Below is the function definition for a function that prints a to z */
+
+/* betty style doc for function main goes there */
+
+/* Function definition for  the function that prints a to z */
+
+/**
+ * print_alphabet - Prints the a to z.
+ *
+ * Return: no return.
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+}
