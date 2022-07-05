00182776d106b28d616d9a0f7e3e0151c92a8d89
Yonas Bekele <yonassbekele@gmail.com>
2022-07-05 04:02:48 -0700

    Is letter lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/* more headers goes there */
+/* Below is the function definition for a function that prints a to z */
+
+/* betty style doc for function main goes there */
+
+/* Function definition for  the function that prints a to z */
+
+/**
+ * print_alphabet_x10 - Prints the a to z 10 times.
+ *
+ * Return: no return.
+ */
+
+int _islower(int c)
+{
+
+	if ((c >=97) && (c <= 122))
+	{
+		return(1);
+	else
+	{
+		return(0);
+	}
+}
