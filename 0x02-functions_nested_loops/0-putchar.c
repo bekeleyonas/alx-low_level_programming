ec121285825665a9ed4ecdab5e87dcd26b3c2a63
Yonas Bekele <yonassbekele@gmail.com>
2022-07-05 03:31:11 -0700

    Header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+/* Below is the function definition for a function that prints a to z */
+
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('H');
+	return (0);
+}
