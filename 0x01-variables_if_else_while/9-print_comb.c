b94b6ef1af09a2a02739615e2e4c0fbb48a5aa1f
Yonas Bekele <yonassbekele@gmail.com>
2022-07-04 19:08:36 -0700

    0, 1, ...9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+		if (ch != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
