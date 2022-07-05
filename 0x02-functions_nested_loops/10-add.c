2dc84a43adb58409f1b1ebb4bfc8d4b1d9eb36cd
Yonas Bekele <yonassbekele@gmail.com>
2022-07-05 12:37:19 -0700

    Sum two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* more headers goes there */
+/* Below is the function definition for a function that prints a to z */
+
+/* betty style doc for function main goes there */
+
+/* Function definition for  the function that prints a to z */
+
+/**
+ * add(int num1, int num2) -  reruns multiplication time table
+ * @num1 : number to add
+ * @num2 : number to add
+ * Return: 1 for lower case or 0 for other.
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+	return (num1 + num2);
+}
