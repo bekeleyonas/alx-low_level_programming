b33c2ce40f7019437af0e814adb0ac78e3d75993
Yonas Bekele <yonassbekele@gmail.com>
2022-07-05 03:38:57 -0700

    Function a to z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,17 +3,6 @@
 /* Below is the function definition for a function that prints a to z */
 
 /* betty style doc for function main goes there */
-/**
- * main - Prints the last digit of a randomly generated number
- *        and whether it is greater than 5, less than 6, or 0.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 
 /* Function definition for  the function that prints a to z */
 
