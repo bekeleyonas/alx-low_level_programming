b8a585907cb88672960dc029557719d455a748ef
Yonas Bekele <yonassbekele@gmail.com>
2022-07-05 05:14:23 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/* more headers goes there */
+/* Below is the function definition for a function that prints a to z */
+
+/* betty style doc for function main goes there */
+
+/* Function definition for  the function that prints a to z */
+
+/**
+ * print_sign -  check if an input is +,- or 0
+ * @n : integer - the character to be checked
+ * Return: 1 for lower case or 0 for other.
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
+}
