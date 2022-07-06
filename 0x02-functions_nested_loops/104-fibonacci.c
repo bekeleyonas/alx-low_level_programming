4fc728f1ee7a0f0d558f3191154fa4388419fe3b
Yonas Bekele <yonassbekele@gmail.com>
2022-07-05 18:23:49 -0700

    First 98 Fab

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the sum of Even Fibonacci numbers
+  * less than 4000000.
+  *
+  * Return: Nothing!
+  */
+int main(void)
+{
+	int i = 2;
+	unsigned long pre = 1, last = 2, sum = last;
+
+	printf("%d, ", pre);
+	printf("%d, ", last);
+
+	while (i <= 98)
+	{
+		sum = last + pre;
+
+		printf("%d, ", sum);
+
+		pre = last;
+		last = sum;
+
+		i = i + 1;
+	}
+
+	printf("\n");
+	return (0);
+}
