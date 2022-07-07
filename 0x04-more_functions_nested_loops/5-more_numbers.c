6106dfb3e028083d524041ebd7d840ed88f6e0da
Yonas Bekele <yonassbekele@gmail.com>
2022-07-07 13:56:45 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * void more_numbers(void) - prints 0 to 9.
+ *
+ *
+ * Return: none
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; c <= 14; c++)
+		{
+			if (j <= 9)
+			{
+				_putchar(c);
+			{
+			else
+			{
+				_putchar(49);
+				_putchar((j%10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
