f667fc12b2ad4d76a1cbbfd30c867943035cb819
Yonas Bekele <yonassbekele@gmail.com>
2022-07-07 13:02:30 -0700

    Print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 to 9.
+ *
+ *
+ * Return: none
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c)
+	}
+	_putchar('\n')'
+}
