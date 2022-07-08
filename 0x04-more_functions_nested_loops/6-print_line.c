ffc052efb0f6d149cb8148d6067fc3c40cd179f3
Yonas Bekele <yonassbekele@gmail.com>
2022-07-07 18:51:16 -0700

    Shortest distance

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line -  print - - n times
+ *
+ * Return: none
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; j <= n; i++)
+		{
+			_putchar('-');
+		}
+	}
+	_putchar('\n');
+}
