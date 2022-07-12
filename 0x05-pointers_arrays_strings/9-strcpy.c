5c7299370e65a357588230f720c2238b26872655
Yonas Bekele <yonassbekele@gmail.com>
2022-07-11 19:53:23 -0700

    Copy String

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,13 +8,11 @@
   */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i;
 
-
-	while (src[i] != '\0')
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 
 	dest[i++] = '\0';
