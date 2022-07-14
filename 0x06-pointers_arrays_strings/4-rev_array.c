7a5ca81f93c2f3b297bf0b2c886edf74b86bdfde
Yonas Bekele <yonassbekele@gmail.com>
2022-07-14 16:49:19 -0700

    Reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * reverse_array -  reverses an array
+  * @a: array to reverse
+  * @n: number of elements
+  * Return: none
+  */
+void reverse_array(int *a, int n)
+{
+	int i = 0, tmpval = 0, lim;
+
+	if (n % 2 == 0)
+	{
+		lim = n / 2;
+	}
+	else
+	{
+		lim = (n - 1) / 2;
+	}
+	for (i = 0; i <= lim; i++)
+	{
+		tmpval = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i]] = tmpval;
+	}
+}
