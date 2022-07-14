15494f7e25d8bb839b30df80d049cf742bcb1112
Yonas Bekele <yonassbekele@gmail.com>
2022-07-14 14:49:00 -0700

    Copy n chars

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,14 +8,15 @@
   */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j, m = n;
+	int j = 0;
 
-	j = 0;
-	while ((src[j] != '\0') && j <= m)
+	while ((src[j] != '\0') && j < n)
 	{
 		dest[j] = src[j];
 		j++;
 	}
-	dest[j] = '\0';
+	for ( ; j < n; j++)
+		dest[j] = '\0';
+
 	return (dest);
 }
