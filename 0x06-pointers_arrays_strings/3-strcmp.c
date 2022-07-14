0853bb04915752ae76c087a0b136f527763623b0
Yonas Bekele <yonassbekele@gmail.com>
2022-07-14 16:30:48 -0700

    Compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,6 @@
   * _strcmp - compares two  strings
   * @s1: string to compare
   * @s2: string to compare against
-  * 
   * Return: 0 for different 1 for match
   */
 int _strcmp(char *s1, char *s2)
@@ -13,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0')
 		i++;
 	n = i;
-	while (s2[i] !='\0')
+	while (s2[i] != '\0')
 		i++;
 	if (i < n)
 		n = i;
