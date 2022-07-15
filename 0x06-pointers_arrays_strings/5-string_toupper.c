097a66bd5b1dd7cb49a280b6f3b09d368928b294
Yonas Bekele <yonassbekele@gmail.com>
2022-07-14 17:24:05 -0700

    To Upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,9 +9,9 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i] !='\0')
+	while (str[i] != '\0')
 	{
-		if (str[i] >=97 && str[i] <= 122)
+		if (str[i] >= 97 && str[i] <= 122)
 			str[i] = str[i] - 32;
 		i++;
 	}
