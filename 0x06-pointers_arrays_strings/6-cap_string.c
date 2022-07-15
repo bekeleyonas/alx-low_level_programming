3af2679f758a71de60dc308cb4678ad1e4484018
Yonas Bekele <yonassbekele@gmail.com>
2022-07-14 18:05:01 -0700

    First letter cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * *cap_string - chars to uppercase
+  * @str: string
+  *
+  * Return:  string
+  */
+char *cap_string(char *str)
+{
+	int i = 0, j;
+	char sep[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j <= 12; j++)
+		{
+			if ((i == 0) || (str[i-1] == sep[j]))
+			{
+				if (str[i] >= 97 && str[i] <= 122)
+					str[i] = str[i] - 32;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
