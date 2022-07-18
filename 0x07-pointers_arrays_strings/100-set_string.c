d9ce7619cdbf65e5c5f1edc2a96efc199289e477
Yonas Bekele <yonassbekele@gmail.com>
2022-07-18 15:50:47 -0700

    Set pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * print_diagsums - Print the sum of diagonal
+  * @a: array of pieces
+  * @size :  size of array
+  * Return: Nothing.
+  */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
