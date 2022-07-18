8f2bee3bfeb7733db3accb9973078c890d8ebd6a
Yonas Bekele <yonassbekele@gmail.com>
2022-07-18 13:56:29 -0700

    Find character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * *_strchr - searched a charater in a string
+  * @s: search target
+  * @c: character to find
+  *
+  *
+  * Return: the memory area filled
+  */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *loc;
+
+	loc = NULL;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			loc = s + i;
+			break;
+		}
+		i++;
+	}
+	return (loc);
+}
