b4dfa811b8eb4e8f04eda8e6278972ab49facb02
Yonas Bekele <yonassbekele@gmail.com>
2022-07-18 15:17:37 -0700

    Chess Board

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * *_strstr - finds a string in a given string  in another string
+  * @heystack: string to search target
+  * @needle: string to find
+  *
+  *
+  * Return: the memory area filled
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0;
+
+	while (heystack[j])
+	{
+		if heystack[j] == needle[0])
+		{
+			i = 1;
+			while (accept[i])
+			{
+			if (s[j] == accept[i])
+				return (s + j);
+			i++;
+		}
+		j++;
+	}
+	return (NULL);
+}
