6cd53482bf94db64331ea9cc55e039339de0640b
Yonas Bekele <yonassbekele@gmail.com>
2022-07-20 10:11:43 -0700

    Prime Recursive

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * _sqrt_recursion - returns the natural square root of a number.
+  * @x: integer
+  * @y: integer
+  *
+  * Return: factoria
+  */
+int _sqrt_recursion(int n)
+{
+	int i, prod;
+
+	for (i = 1; i <= n/2; i++)
+	{
+		prod = i * i;
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
