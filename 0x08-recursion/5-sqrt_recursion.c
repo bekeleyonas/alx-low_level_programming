0d471b9475c8e7084a4ce3b7d37699227921b16e
Yonas Bekele <yonassbekele@gmail.com>
2022-07-20 10:21:31 -0700

    Square root  Recursive

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,32 @@
 #include "main.h"
 /**
   * _sqrt_recursion - returns the natural square root of a number.
-  * @x: integer
-  * @y: integer
+  * @n: ineger
   *
   * Return: factoria
   */
 int _sqrt_recursion(int n)
 {
-	int i, prod;
 
-	for (i = 1; i <= n/2; i++)
-	{
-		prod = i * i;
-	if (y < 0)
+	return (getsqrt(n, 1));
+
+}
+/**
+  * getsqrt - gets square root
+  * @n: number to calculate the square root
+  * @i: counter number
+  *
+  * Return: the natural square root
+  */
+int getsqrt(int n, int i)
+{
+	int sqrt = i * i;
+
+	if (sqrt > n)
 		return (-1);
-	else if (y == 0)
-		return (1);
-	else
-		return (x * _pow_recursion(x, y - 1));
+
+	if (sqrt == n)
+		return (i);
+
+	return (getsqrt(n, i + 1));
 }
