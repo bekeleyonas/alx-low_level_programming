49a30db71e57829e7e1ad1789d2f3d689f0a04a0
Yonas Bekele <yonassbekele@gmail.com>
2022-07-20 12:32:01 -0700

    Palindr

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,12 +9,22 @@ int is_palindrome(char *s)
 {
 	int n = 0;
 
-	while (s[n] != '\0')
-	{
-		n++;
-	}
-	n--;
+	n = countlength(s, 0);
 	return (checkpalind(s, n));
+}
+/**
+ * countlength - obtains length of a
+ * @str: string
+ * @n: integer to count length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int countlength(char *str, int n)
+{
+	if (*str == 0)
+		return (n - 1);
+	return (countlength(str + 1, n + 1));
 }
 /**
  * checkpalind - compares string with the far end
